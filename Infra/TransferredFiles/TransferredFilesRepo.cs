e116da518c2124ed8b5efc971dee0f799a032d2c
zevspilman <zspilman@spearhead-software.com>
2024-12-19 11:56:49 -0500

    added tenant repo

---
+++ Infra/TransferredFiles/TransferredFilesRepo.cs
@@ -0,0 +1,32 @@
+﻿using App.TransferredFiles;
+using Domain.TransferredFiles;
+using Infra.Common.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.TransferredFiles
+{
+    public class TransferredFilesRepo(AppDBContext dbContext) : ITransferredFileRepo
+    {
+        private readonly AppDBContext _db = dbContext;
+
+        public async Task AddAsync(TransferredFile file)
+        {
+            await _db.TransferredFiles.AddAsync(file);
+        }
+
+        public async Task AddCollectionAsync(List<TransferredFile> files)
+        {
+            await _db.TransferredFiles.AddRangeAsync(files);
+        }
+
+        public async Task<TransferredFile?> GetByIdAsync(Guid id)
+        {
+            return await _db.TransferredFiles.SingleAsync(tf => tf.Id == id);
+        }
+
+        public async Task<List<TransferredFile>?> GetListAsync()
+        {
+            return await _db.TransferredFiles.ToListAsync();
+        }
+    }
+}
